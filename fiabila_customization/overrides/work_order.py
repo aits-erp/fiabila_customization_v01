be3cea6470c5708c788ead99323655a3a1dddee5
uNik020 <shekharnikhil020@gmail.com>
2026-03-23 09:40:27 +0000

    validate fixed

--- fiabila_customization/overrides/work_order.py
+++ fiabila_customization/overrides/work_order.py
@@ -1,26 +1,28 @@
-# fiabila_customization/overrides/work_order.py
+# # fiabila_customization/overrides/work_order.py
 
 import frappe
 from erpnext.manufacturing.doctype.work_order.work_order import WorkOrder as ERPNextWorkOrder
 
-
 class CustomWorkOrder(ERPNextWorkOrder):
 
-    def before_save(self):
-        # super().before_save()  # Let ERPNext do everything first Crashes
-        self.apply_custom_warehouse_mapping()  # Then overwrite with BOM custom fields
+    def validate(self):
+        # Let ERPNext do all its internal calculations first
+        super().validate()
+
+        # Apply your custom logic AFTER standard behavior
+        self.apply_custom_warehouse_mapping()
 
     def apply_custom_warehouse_mapping(self):
         if not self.bom_no:
             return
 
-        bom = frappe.get_doc("BOM", self.bom_no)
+        bom = frappe.get_cached_doc("BOM", self.bom_no)
 
         source_wh = bom.get("custom_source_warehouse")
         wip_wh = bom.get("custom_workinprogress_warehouse")
         fg_wh = bom.get("custom_target_warehouse")
 
-        # Only override if BOM has custom values
+        # Set only if present in BOM
         if source_wh:
             self.source_warehouse = source_wh
         if wip_wh:
@@ -28,11 +30,54 @@ class CustomWorkOrder(ERPNextWorkOrder):
         if fg_wh:
             self.fg_warehouse = fg_wh
 
-        # Push source_warehouse down to required_items child table
-        final_source = self.source_warehouse
-        if final_source:
+        # Apply to child table WITHOUT overriding user edits
+        if self.source_warehouse:
             for row in self.required_items:
-                row.source_warehouse = final_source
+                # Only set if empty OR if you want strict control, remove condition
+                if not row.source_warehouse:
+                    row.source_warehouse = self.source_warehouse
+
+
+
+# import frappe
+# from erpnext.manufacturing.doctype.work_order.work_order import WorkOrder as ERPNextWorkOrder
+
+
+# class CustomWorkOrder(ERPNextWorkOrder):
+
+#     def before_save(self):
+#         # super().before_save()  # Let ERPNext do everything first Crashes
+#         self.apply_custom_warehouse_mapping()  # Then overwrite with BOM custom fields
+
+#     def apply_custom_warehouse_mapping(self):
+#         if not self.bom_no:
+#             return
+
+#         bom = frappe.get_doc("BOM", self.bom_no)
+
+#         source_wh = bom.get("custom_source_warehouse")
+#         wip_wh = bom.get("custom_workinprogress_warehouse")
+#         fg_wh = bom.get("custom_target_warehouse")
+
+#         # Only override if BOM has custom values
+#         if source_wh:
+#             self.source_warehouse = source_wh
+#         if wip_wh:
+#             self.wip_warehouse = wip_wh
+#         if fg_wh:
+#             self.fg_warehouse = fg_wh
+
+#         # Push source_warehouse down to required_items child table
+#         final_source = self.source_warehouse
+#         if final_source:
+#             for row in self.required_items:
+#                 row.source_warehouse = final_source
+
+
+
+
+
+
 
 # import frappe
 # from erpnext.manufacturing.doctype.work_order.work_order import WorkOrder as ERPNextWorkOrder
